7cc711aae61bb6380038ac2145849ea65d1a6984
Carlos Sosa <carlangas10001@gmail.com>
2021-08-27 14:48:35 -0500

    dark mode con localstorage

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const d=document
+//VARIABLES
+const d=document,
+darkModeBtn = d.querySelector(".darkmode-btn"),
+html = d.querySelector("html"),
+currentTheme = localStorage.getItem("theme");
+//EVENT LISTENERS
+d.addEventListener("DOMContentLoaded", ()=>{
+ if (currentTheme == "dark") {
+  html.classList.add("dark")
+ }
+ darkModeBtn.addEventListener('click',darkMode)
+})
+//FUNCTIONS
+function darkMode(e){
+ html.classList.toggle("dark");
+ let theme = "light";
+ if (html.classList.contains("dark")) {
+   theme = "dark";
+ }
+ localStorage.setItem("theme", theme);
+}
+
+
