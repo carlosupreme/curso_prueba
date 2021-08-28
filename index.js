60fe57bda88f03bc03401de5dda513f889440d56
Carlos Sosa <carlangas10001@gmail.com>
2021-08-28 12:20:07 -0500

    Random feature

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 //VARIABLES
 const d=document,
 darkModeBtn = d.querySelector(".darkmode-btn"),
-html = d.querySelector("html")
+html = d.querySelector("html"),
+randomBtn = d.querySelector("#random"),
+randomSection = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 //EVENT LISTENERS
 d.addEventListener("DOMContentLoaded", ()=>{
  darkModeBtn.addEventListener('click',darkMode)
+ randomBtn.addEventListener('click', getRandomSection)
 })
+
 //FUNCTIONS
 function darkMode(e){
  e.preventDefault()
  html.classList.toggle("dark");
 }
 
-
+function getRandomSection(){
+  let seccion = Math.round((Math.random())*14)+1
+  let href = `#section${seccion}`
+  randomBtn.setAttribute('href',href)
+}
