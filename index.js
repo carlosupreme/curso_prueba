0a262fb0cf40e79f01bb652b44e664c14dcccdba
Carlos Sosa <carlangas10001@gmail.com>
2021-09-01 20:56:35 -0500

    search feature test

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ randomBtn = d.querySelector("#random"),
 searchBtn = d.querySelector("#searchBtn"),
 searchContainer = d.querySelector(".search-container"),
 footer = d.querySelector("footer"),
-buscarEnSearch = d.querySelector(".search-input span"),
-inputEnSearch = d.querySelector(".search-input input")
+buscarEnSearch = d.querySelector(".search-input a"),
+inputEnSearch = d.querySelector(".search-input input"),
+sectionsNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] 
 //EVENT LISTENERS
 d.addEventListener("DOMContentLoaded", ()=>{
  darkModeBtn.addEventListener('click',darkMode)
  randomBtn.addEventListener('click', getRandomSection)
  searchBtn.addEventListener('click', deploySearchContainer)
  buscarEnSearch.addEventListener('click', buttonOnclick)
+ inputEnSearch.addEventListener('input', searching)
 })
 //FUNCTIONS
 function darkMode(e){
@@ -36,7 +38,21 @@ function deploySearchContainer(){
 function buttonOnclick(){
   buscarEnSearch.classList.add("onclick")
   searchContainer.classList.toggle("is-active")
-  setTimeout(()=>{
-    inputEnSearch.value = ""
-  },300)
+}
+function searching(){
+  let value = inputEnSearch.value
+  value.toLowerCase()
+  value.trim()
+  let x = 'section'
+  const sections = [`${x}1`,`${x}2`,`${x}3`,`${x}4`,`${x}5`,`${x}6`,`${x}7`,`${x}8`,`${x}9`,`${x}10`,`${x}11`,`${x}12`,`${x}13`,`${x}14`,`${x}15`]
+  const unaSection = sections.splice(0,sections.length)
+  for (let i = 0; i < unaSection.length; i++) {
+    if(value.includes(unaSection[i])){
+      buscarEnSearch.setAttribute('href',`#${value}`)
+      setTimeout(()=>buscarEnSearch.click(),500)
+    }else{
+      break
+    }
+  }
+  // console.log('ayuda')
 }
