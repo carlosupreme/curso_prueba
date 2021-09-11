5b504ca12c1086190082bf4e8a318dd6a19caa5a
Carlos Sosa <carlangas10001@gmail.com>
2021-09-11 13:15:41 -0500

    menu styles

--- js/section2.js
+++
@@ -1,7 +0,0 @@
-const d = document,
-  $playBtn = d.querySelector(".play"),
-  $pauseBtn = d.querySelector(".pause"),
-  $restartBtn = d.querySelector(".restart"),
-  $minutes = d.querySelector(".mins"),
-  $seconds = d.querySelector(".secs"),
-  $miliseconds = d.querySelector(".ms");
