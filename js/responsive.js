d284b22dc0ffbca4f85ab04520e473d58c248ca6
Carlos Sosa <carlangas10001@gmail.com>
2021-09-12 09:35:33 -0500

    section8 responsive tester

--- js/responsive.js
+++ js/responsive.js
@@ -1,13 +1,30 @@
 const d = document,
   w = window;
-
+const cl = console.log;
 export default function responsive(id, mq, mobile, desktop) {
   let breakpoint = w.matchMedia(mq);
   const responsive = (e) => {
     if (e.matches) d.getElementById(id).innerHTML = mobile;
     else d.getElementById(id).innerHTML = desktop;
-    console.log(e, breakpoint);
-   };
+  };
   breakpoint.addEventListener("change", responsive);
-  responsive(breakpoint)
+  responsive(breakpoint);
+}
+
+export function responsiveTester(form) {
+  const $form = d.getElementById(form)
+  let ventana
+  d.addEventListener("submit", (e) => { 
+    if (e.target === $form) {
+      e.preventDefault();
+       ventana =  w.open(
+         $form.direction.value,
+         "ventana",
+        `innerWidth=${$form.width.value}, innerHeight=${$form.height.value}`
+      )
+      d.addEventListener('click',e=>{
+        if(e.target === $form.close) ventana.close()
+      })
+    }
+  });
 }
