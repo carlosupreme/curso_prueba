6f55c0326558e030eb21af0bf8fb1824954c0cba
Carlos Sosa <carlangas10001@gmail.com>
2021-09-13 12:40:52 -0500

    adding options in getCurrentPosition

--- js/geolocation.js
+++ js/geolocation.js
@@ -8,22 +8,29 @@ export default function getLocation(container) {
       const $mapLink = d.createElement("a"),
         latitud = position.coords.latitude,
         longitud = position.coords.longitude,
+        precision = position.coords.accuracy,
         maps = [latitud, longitud].join();
-
       $container.innerHTML = `
       <ul class="section">
         <li class="title">Longitud: <span class="text-main">${longitud}</span> </li>
         <li class="title">Latitud: <span class="text-main">${latitud}</span></li>
+        <li class="title">Precisión: <span class="text-main">${precision} mts.</span></li>
       </ul>`;
 
       $mapLink.textContent = "Ver en Google Maps... 👀👀👀";
       $mapLink.target = "blank";
-      $mapLink.href = `https://www.google.com.mx/maps/@${maps}`;
+      $mapLink.href = `https://www.google.com.mx/maps/@${maps},20z`;
       $mapLink.classList.add("title", "first");
       $container.append($mapLink);
     };
     const error = (e) => $container.innerHTML = `<h3 class="text-main first">Parece ser que alguien no quiere dar su ubicación 😒</h3>`;
-    n.geolocation.getCurrentPosition(success, error);
+
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximunAge: 0
+    }
+    n.geolocation.getCurrentPosition(success, error,options);
   } else {
     /* geolocation is not available */
     $container.innerHTML = `<h3 class="text-main first">Parece ser que alguien no quiere dar su ubicación 😒</h3>`;
