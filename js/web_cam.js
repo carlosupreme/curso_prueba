cebc08c4e07d77fe1ee2f39588ee3f2e2066c735
Carlos Sosa <carlangas10001@gmail.com>
2021-09-12 22:37:12 -0500

    message on video plays

--- js/web_cam.js
+++ js/web_cam.js
@@ -1,17 +1,22 @@
 const cl = console.log;
-export default function getCamara(container,error) {
+export default function getCamara(container, error) {
   const $video = document.getElementById(container),
-  $message = document.querySelector(error)
+    $message = document.querySelector(error);
   if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices
       .getUserMedia({ video: true })
-      .then((stream)=> {
+      .then((stream) => {
         $video.srcObject = stream;
+        setTimeout(() => {
+          $message.innerHTML = `OMG you so ugly 🤮🤮🤮🤮🤮`;
+          $message.style.display = "block";
+          $message.classList.add("title");
+        }, 3000);
         // $video.play() -------> tambien puede ser, pero use autoplay en HTML
       })
-      .catch((err) =>{
-       $video.style.display = 'none'
-        $message.style.display = 'block'
+      .catch((err) => {
+        $video.style.display = "none";
+        $message.style.display = "block";
       });
   }
 }
