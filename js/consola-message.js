6e25a04e0cd62a669c282b0c2e7e0bfef0a5a920
Carlos Sosa <carlangas10001@gmail.com>
2021-09-14 14:52:09 -0500

    slider

--- js/consola-message.js
+++ js/consola-message.js
@@ -1,3 +1,3 @@
 export default function consolaCSS(){
- console.log(`%c Hola amigo developer`,"color:#D4FF00; font-size: 40px;")
+ console.log(`%c Hola amigo developer`,"color: #D4FF00; font-size: 40px;")
 }
