0da12b08cbc423679e57b0760f31cc36817d7a24
Carlos Sosa <carlangas10001@gmail.com>
2022-02-10 14:28:14 -0600

    agregue identacion y un fondo a la consola

--- js/consola-message.js
+++ js/consola-message.js
@@ -1,3 +1,3 @@
 export default function consolaCSS(msg){
- console.log(`%c${msg}`,"color: #D4FF00; font-size: 40px;")
+ console.log(`%c${msg}`,"color: #D4FF00; font-size: 40px; background-color:#3b3a37;")
 }
